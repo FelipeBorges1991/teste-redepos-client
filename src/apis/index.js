3acd9715f45b38a113ec01617c83a35bbf31a9de
Felipe Borges <fcborges_@hotmail.com>
2021-07-16 16:02:01 -0300

    Configuração final para deploy no Netilify+Heroku

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const apis ={
+  development: "http://localhost:4000",
+  production: "https://classroom-redepos.herokuapp.com"
+}
+
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: apis[process.env.NODE_ENV],
 });
 
 export default api;
